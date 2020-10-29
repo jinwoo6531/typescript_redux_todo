18c7926d036d00e8df8808b9ccfd2c0e85d48273
jinwoo <jintakk6531@gmail.com>
2020-10-29 19:33:41 +0900

    10/29 삭제 action 추가

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -11,9 +11,24 @@ interface AppProps {
 
 
 export class _App extends React.Component<AppProps> {
+    
+    onButtonClick = ():void => {
+        this.props.fetchTodos();
+    }
+
+    renderList(): JSX.Element[] {
+        return this.props.todos.map((todo:Todo) => {
+            return <div key={todo.id}>{todo.title}</div>
+        })
+    }
     render() {
 
-        return <div>Hi</div>
+        return (
+            <div>
+                <button onClick={this.onButtonClick}>Fetch</button>
+                {this.renderList()}
+            </div>
+        )
     }
 }
 
--- src/reducers/todos.ts
+++ src/reducers/todos.ts
@@ -1,11 +1,13 @@
-import { Todo,FetchTodosAction } from "../actions";
-import { ActionTypes } from "../actions/types";
+import { Todo, ActionTypes } from "../actions";
+import { Action} from '../actions/types'
 
 
-export const todosReducer = (state: Todo[] = [],action: FetchTodosAction) => {
+export const todosReducer = (state: Todo[] = [],action: Action ) => {
     switch (action.type) {
         case ActionTypes.fetchTodos:
             return action.payload;               
+        case ActionTypes.deleteTodo:
+            return 
         default:
             return state;
     }
